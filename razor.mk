805154ffde87983d3a5e4ba835d84445010a4c83
Gabriel Horn <gabriel_horn@web.de>
2016-01-11 11:35:33 +0100

    Update and rename cm.mk to razor.mk

--- razor.mk
+++ razor.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := awifi
-PRODUCT_NAME := cm_awifi
+PRODUCT_NAME := razor_awifi
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-V500
 PRODUCT_MANUFACTURER := LGE
